8fb70fbaaecc377f77ea09937ad8fa6e013ce3c2
lifeoncode <nduduzo@yellogarden.co.za>
2022-11-16 23:48:05 +0200

    work title

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,31 +4,34 @@ const Work = () => {
   return (
     <section id="work" className="work">
       <div className="container">
-        <a href="https://darkyntel.com" rel="noreferrer" target="_blank">
-          <img
-            src={require("../img/shot.jpg")}
-            data-rate="0.1"
-            alt="Darkyntel"
-          />
-        </a>
-        <a
-          href="https://fasttypingfingers.vercel.app"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <img
-            src={require("../img/shot-2.jpg")}
-            data-rate="0.4"
-            alt="Typing app"
-          />
-        </a>
-        <a href="https://yellogarden.co.za" rel="noreferrer" target="_blank">
-          <img
-            src={require("../img/shot-3.png")}
-            data-rate="0.2"
-            alt="Yello Garden"
-          />
-        </a>
+        <h1>My recent work</h1>
+        <div className="work-inner">
+          <a href="https://darkyntel.com" rel="noreferrer" target="_blank">
+            <img
+              src={require("../img/shot.jpg")}
+              data-rate="0.1"
+              alt="Darkyntel"
+            />
+          </a>
+          <a
+            href="https://fasttypingfingers.vercel.app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              src={require("../img/shot-2.jpg")}
+              data-rate="0.4"
+              alt="Typing app"
+            />
+          </a>
+          <a href="https://yellogarden.co.za" rel="noreferrer" target="_blank">
+            <img
+              src={require("../img/shot-3.png")}
+              data-rate="0.2"
+              alt="Yello Garden"
+            />
+          </a>
+        </div>
       </div>
     </section>
   );
