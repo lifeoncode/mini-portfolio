4430635cf669cc6c12314cdddbf39b6b1f2f7fdc
lifeoncode <nduduzo@yellogarden.co.za>
2022-11-17 03:05:15 +0200

    closing

---
+++ src/components/Closing.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Closing = () => {
+  return (
+    <section className="closing" id="closing">
+      <div className="container">
+        <h3>
+          According to the research I've been conducting since I started my job
+          hunt, no other company comes close to what Sovtech offers. The
+          possibility of working at Sovtech sounds like a dream to me.
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default Closing;
